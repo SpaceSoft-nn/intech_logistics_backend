8449a7c01732922039d246f95fdb143c4f9449e3
Xpyst <qjq3@mail.ru>
2024-09-25 22:24:46 +0300

    для сваггера нужна зависимость поэтому phpunit/phpunit: ^11.0.1 перевёл в non-dev

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Deploying..."
 
 php8.3 artisan down
 
-php8.3 composer.phar install --optimize-autoloader
+php8.3 composer.phar install --no-dev --optimize-autoloader
 
 php8.3 artisan migrate --force
 

1ffa400e3172df35e46f06a13b2c63e83cb79172
Xpyst <qjq3@mail.ru>
2024-12-24 21:03:05 +0300

    вернул сервес провайдеры

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,27 +5,28 @@ return [
     App\Providers\AppServiceProvider::class,
 
         #Custom Service
-    // App\Modules\Notification\App\Providers\Notification\NotificationServiceProvider::class,
-    // App\Modules\User\App\Providers\UserServiceProvider::class,
-    // App\Modules\Organization\App\Providers\OrganizationServiceProvider::class,
-    // App\Modules\InteractorModules\Registration\App\Providers\RegistrationServiceProvider::class, //сервес объединение User + Organization
-    // App\Modules\IndividualPeople\App\Providers\IndividualPeopleServiceProvider::class,
-    // App\Modules\Permission\App\Providers\PerrmissionServoceProvider::class, // Сервес прав по бинарной таблице
-    // App\Modules\Transport\App\Providers\TransportServiceProvider::class,
-    // App\Modules\Address\App\Providers\AddressServiceProvider::class,
-    // App\Modules\OrderUnit\App\Providers\OrderUnitServiceProvider::class,
-    // App\Modules\Transfer\App\Providers\TransferServiceProvider::class,
-    // App\Modules\Matrix\App\Providers\MatrixServiceProvider::class,
-    // App\Modules\GAR\App\Providers\GARServiceProvider::class,
-    // App\Modules\InteractorModules\AgreementTransfer\App\Providers\AgreementTransferServiceProvider::class,
-    // App\Modules\OfferContractor\App\Providers\OfferContractorServiceProvider::class,
+    App\Modules\Notification\App\Providers\Notification\NotificationServiceProvider::class,
+    App\Modules\User\App\Providers\UserServiceProvider::class,
+    App\Modules\Organization\App\Providers\OrganizationServiceProvider::class,
+    App\Modules\InteractorModules\Registration\App\Providers\RegistrationServiceProvider::class, //сервес объединение User + Organization
+    App\Modules\IndividualPeople\App\Providers\IndividualPeopleServiceProvider::class,
+    App\Modules\Permission\App\Providers\PerrmissionServoceProvider::class, // Сервес прав по бинарной таблице
+    App\Modules\Transport\App\Providers\TransportServiceProvider::class,
+    App\Modules\Address\App\Providers\AddressServiceProvider::class,
+    App\Modules\OrderUnit\App\Providers\OrderUnitServiceProvider::class,
+    App\Modules\Transfer\App\Providers\TransferServiceProvider::class,
+    App\Modules\Matrix\App\Providers\MatrixServiceProvider::class,
+    App\Modules\GAR\App\Providers\GARServiceProvider::class,
+    App\Modules\InteractorModules\AgreementTransfer\App\Providers\AgreementTransferServiceProvider::class,
+    App\Modules\OfferContractor\App\Providers\OfferContractorServiceProvider::class,
 
-    //     #Interactor Service
-    // App\Modules\InteractorModules\AddressOrder\App\Providers\AddressOrderServiceProvide::class,
-    // App\Modules\InteractorModules\OrganizationOrderInvoice\App\Providers\OrganizationOrderInvoiceServiceProvider::class,
+        #Interactor Service
+    App\Modules\InteractorModules\AddressOrder\App\Providers\AddressOrderServiceProvide::class,
+    App\Modules\InteractorModules\OrganizationOrderInvoice\App\Providers\OrganizationOrderInvoiceServiceProvider::class,
 
 
-    //     #Auth Service
-    // App\Modules\Auth\App\Providers\AuthServiceProvider::class,
+        #Auth Service
+    App\Modules\Auth\App\Providers\AuthServiceProvider::class,
 
 ];
+ва

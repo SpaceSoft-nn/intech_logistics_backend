7c2bff6c6fa350a16515eef6de3e7b058ab87cd2
Xpyst <qjq3@mail.ru>
2024-12-27 03:51:44 +0300

    добавил задачу для кроны промежуток 5 минут

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('email:parse-status')->everyMinute();
+Schedule::command('email:parse-status')->everyFiveMinutes();

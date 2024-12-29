37e26081180794d39b55b2e658ff1f6300f255ef
Xpyst <qjq3@mail.ru>
2024-12-29 22:03:40 +0300

    работ над созданием заказов

--- app/Modules/Tender/Domain/Interactor/AgreementTenderAcceptInteractor.php
+++ app/Modules/Tender/Domain/Interactor/AgreementTenderAcceptInteractor.php
@@ -4,10 +4,12 @@ namespace App\Modules\Tender\Domain\Interactor;
 
 use App\Modules\OrderUnit\App\Data\DTO\OrderUnit\OrderUnitCreateDTO;
 use App\Modules\OrderUnit\App\Data\DTO\ValueObject\OrderUnit\OrderUnitVO;
+use App\Modules\OrderUnit\App\Data\Enums\StatusOrderUnitEnum;
 use App\Modules\OrderUnit\Domain\Services\OrderUnitService;
 use App\Modules\Tender\Domain\Models\LotTender;
 use App\Modules\Tender\Domain\Models\Response\AgreementTender;
 use App\Modules\Tender\Domain\Models\Response\AgreementTenderAccept;
+use DB;
 use Illuminate\Support\Carbon;
 
 // Бизнес логика на согласование Тендера с двух сторон - здесь же создаются заказы (pre-order)
@@ -51,44 +53,83 @@ final class AgreementTenderAcceptInteractor
 
                 $dates = $lot_tender->specifica_date_period;
 
-                //проходим по дате
-                foreach ($dates as $date) {
 
-                    { //Вычситывает дату
-                        $carbon_date_start = Carbon::createFromFormat('d,m,Y', $date);
+                DB::transaction(function () use ($dates, $lot_tender, $agreement_tender) {
 
-                        //получем конечную дату в зависимости от периода #TODO Потом нужно устанавливать в зависимости от километража
-                        $carbon_date_end = $carbon_date_start->addDays($lot_tender->period);
+                    //проходим по дате
+                    foreach ($dates as $date) {
+
+                        { //Высчитывает дату
+                            $carbon_date_start = Carbon::createFromFormat('d,m,Y', $date);
+
+                            //получем конечную дату в зависимости от периода #TODO Потом нужно устанавливать в зависимости от километража
+                            $carbon_date_end = $carbon_date_start->addDays($lot_tender->period);
+                        }
+
+                        //у каждой даты, есть количество транспорта, который равен: транспорт = заказ
+                        for ($i = 0; $i < $date->count_transport; $i++) {
+
+
+                            $orderUnitVO = OrderUnitVO::make(
+                                end_date_order: $carbon_date_end->toDateString(),
+                                body_volume: $lot_tender->body_volume_for_order,
+                                order_status: StatusOrderUnitEnum::pre_order->value,
+                                order_total: $lot_tender->price_for_km,
+                                type_transport_weight: $lot_tender->type_transport_weight->value,
+                                type_load_truck: $lot_tender->type_load_truck->value,
+                                organization_id: $lot_tender->organization_id,
+                                user_id: null, #TODO Продумать логику при ролях, как указывать правильно
+                                contractor_id: $agreement_tender->organization_contractor_id, //указываем подрядчика на выполнения заказа
+                                lot_tender_id: $lot_tender->id,
+                                add_load_space: false, #TODO Продумать что тут указывать?
+                            );
+
+
+                            //создаём заказы
+                            $this->orderUnitService->createOrderUnit(
+                                dto: OrderUnitCreateDTO::make(orderUnitVO: $orderUnitVO)
+                            );
+
+                        }
+
                     }
 
-                    //у каждой даты, есть количество транспорта, который равен: транспорт = заказ
-                    for ($i = 0; $i < $date->count_transport; $i++) {
+                });
 
 
-                        $orderUnitVO = OrderUnitVO::make(
-                            end_date_order: $carbon_date_end->toDateString(),
-                            body_volume: $lot_tender->body_volume_for_order,
-                            order_total: $lot_tender->price_for_km,
-                            type_transport_weight: $lot_tender->type_transport_weight->value,
-                            type_load_truck: $lot_tender->type_load_truck->value,
-                            organization_id: $lot_tender->organization_id,
-                            user_id: null, #TODO Продумать логику при ролях, как указывать правильно
-                            contractor_id: $agreement_tender->organization_contractor_id, //указываем подрядчика на выполнения заказа
-                            lot_tender_id: $lot_tender->id,
-                            add_load_space: false, #TODO Продумать что тут указывать?
-                        );
+            } else { //если нету конкретных дат
 
+                { //Высчитывает дату
+                    $carbon_date_start = Carbon::createFromFormat('d,m,Y', $lot_tender->date_start);
 
-                        //создаём заказы
-                        $this->orderUnitService->createOrderUnit(
-                            dto: OrderUnitCreateDTO::make(orderUnitVO: $orderUnitVO)
-                        );
-
-                    }
-
+                    //получем конечную дату в зависимости от периода #TODO Потом нужно устанавливать в зависимости от километража
+                    $carbon_date_end = $carbon_date_start->addDays($lot_tender->period);
                 }
 
+                $orderUnitVO = OrderUnitVO::make(
+                    end_date_order: $carbon_date_end->toDateString(),
+                    body_volume: $lot_tender->body_volume_for_order,
+                    order_status: StatusOrderUnitEnum::pre_order->value,
+                    order_total: $lot_tender->price_for_km,
+                    type_transport_weight: $lot_tender->type_transport_weight->value,
+                    type_load_truck: $lot_tender->type_load_truck->value,
+                    organization_id: $lot_tender->organization_id,
+                    user_id: null, #TODO Продумать логику при ролях, как указывать правильно
+                    contractor_id: $agreement_tender->organization_contractor_id, //указываем подрядчика на выполнения заказа
+                    lot_tender_id: $lot_tender->id,
+                    add_load_space: false, #TODO Продумать что тут указывать?
+                );
+
+
+                //создаём заказы
+                $this->orderUnitService->createOrderUnit(
+                    dto: OrderUnitCreateDTO::make(orderUnitVO: $orderUnitVO)
+                );
+
+
             }
+
+
         }
 
 

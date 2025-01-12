3c0c6a5bb0591e7202452b1cf53edb658bec1ebc
Xpyst <qjq3@mail.ru>
2025-01-12 20:10:38 +0300

    добавлены доп поля в TenderVO

--- app/Modules/Tender/App/Data/ValueObject/LotTenderVO.php
+++ app/Modules/Tender/App/Data/ValueObject/LotTenderVO.php
@@ -5,6 +5,8 @@ namespace App\Modules\Tender\App\Data\ValueObject;
 use App\Modules\Base\Traits\FilterArrayTrait;
 use App\Modules\OrderUnit\App\Data\Enums\TypeLoadingTruckMethod;
 use App\Modules\OrderUnit\App\Data\Enums\TypeTransportWeight;
+use App\Modules\Tender\App\Data\Enums\StatusTenderEnum;
+use App\Modules\Tender\App\Data\Enums\TypeTenderEnum;
 use Illuminate\Contracts\Support\Arrayable;
 use Arr;
 
@@ -16,11 +18,16 @@ final readonly class LotTenderVO implements Arrayable
         public string $general_count_transport,
         public float $price_for_km,
         public float $body_volume_for_order,
+
         public TypeTransportWeight $type_transport_weight,
         public TypeLoadingTruckMethod $type_load_truck,
+        public TypeTenderEnum $type_tender,
+
         public string $date_start,
         public int $period,
         public string $organization_id,
+
+        public ?StatusTenderEnum $status_tender,
         public ?int $day_period,
     ) {}
 
@@ -35,6 +42,9 @@ final readonly class LotTenderVO implements Arrayable
         string $date_start,
         int $period,
         string $organization_id,
+        string $type_tender,
+
+        ?StatusTenderEnum $status_tender = null,
         ?int $day_period = null,
 
     ) : self {
@@ -43,11 +53,16 @@ final readonly class LotTenderVO implements Arrayable
             general_count_transport: $general_count_transport,
             price_for_km: $price_for_km,
             body_volume_for_order: $body_volume_for_order,
+
             type_transport_weight: TypeTransportWeight::stringByCaseToObject($type_transport_weight),
             type_load_truck: TypeLoadingTruckMethod::stringByCaseToObject($type_load_truck),
+            type_tender: TypeTenderEnum::stringByCaseToObject($type_tender),
+
             date_start: $date_start,
             period: $period,
             organization_id: $organization_id,
+
+            status_tender: $status_tender,
             day_period: $day_period,
         );
 
@@ -59,12 +74,17 @@ final readonly class LotTenderVO implements Arrayable
         return [
             "general_count_transport" => $this->general_count_transport,
             "price_for_km" => $this->price_for_km,
+
             "body_volume_for_order" => $this->body_volume_for_order,
             "type_transport_weight" => $this->type_transport_weight,
+            "type_tender" => $this->type_tender,
+
             "type_load_truck" => $this->type_load_truck,
             "date_start" => $this->date_start,
             "period" => $this->period,
             "organization_id" => $this->organization_id,
+
+            "status_tender" => $this->status_tender,
             "day_period" => $this->day_period,
         ];
     }
@@ -78,9 +98,13 @@ final readonly class LotTenderVO implements Arrayable
             body_volume_for_order: Arr::get($data, 'body_volume_for_order'),
             type_transport_weight: Arr::get($data, 'type_transport_weight'),
             type_load_truck: Arr::get($data, 'type_load_truck'),
+            type_tender: Arr::get($data, 'type_tender'),
+
             date_start: Arr::get($data, 'date_start'),
             period: Arr::get($data, 'period'),
             organization_id: Arr::get($data, 'organization_id'),
+
+            status_tender: Arr::get($data, 'status_tender', null),
             day_period: Arr::get($data, 'day_period', null),
         );
     }

32b61118df196411d2a1e5128e09f99f6484cfb5
Xpyst <qjq3@mail.ru>
2025-01-30 13:58:03 +0300

    в AgreementTenderResource - изменил на возврат полной модели организации

--- app/Modules/Tender/Domain/Resources/Response/AgreementTenderResource.php
+++ app/Modules/Tender/Domain/Resources/Response/AgreementTenderResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Modules\Tender\Domain\Resources\Response;
 
+use App\Modules\Organization\Domain\Models\Organization;
+use App\Modules\Organization\Domain\Resources\OrganizationResource;
 use App\Modules\Tender\Domain\Resources\LotTenderResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Request;
@@ -16,7 +18,7 @@ class AgreementTenderResource extends JsonResource
 
             "id_areement_tender" => $this->id,
             "lot_tender_response_id" => LotTenderResponseResource::make($this->lot_tender_response),
-            "organization_tender_create_id" => $this->organization_tender_create_id,
+            "organization_tender_create_id" => OrganizationResource::make(Organization::find($this->organization_tender_create_id)),
             "lot_tender_id" => LotTenderResource::make($this->lot_tender),
             "agreement_tender_accept_id" => AgreementTenderAcceptResource::make($this->agreement_tender_accept),
 

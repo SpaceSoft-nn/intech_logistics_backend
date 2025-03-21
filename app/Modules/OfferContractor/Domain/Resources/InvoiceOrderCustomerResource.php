7233b156433f142553962b0373fe33da0ed43332
Xpyst <qjq3@mail.ru>
2025-03-21 21:23:59 +0300

    поправил гнейминг в InvoiceOrderCustomerResource

--- app/Modules/OfferContractor/Domain/Resources/InvoiceOrderCustomerResource.php
+++ app/Modules/OfferContractor/Domain/Resources/InvoiceOrderCustomerResource.php
@@ -22,7 +22,7 @@ class InvoiceOrderCustomerResource extends JsonResource
             "end_address_id" => AddressResource::make($this->end_address),
             "start_date" => $this->start_date,
             "end_date" => $this->end_date,
-            "cargo_good" => $this->cargo_good,
+            "goods_array" => $this->cargo_good,
             "organization_creator_id" => $this->organization_creator_id,
 
         ];
